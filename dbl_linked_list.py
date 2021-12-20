478a5c928f28d7c680c3bfcf6509ba79a2f52910
Eduard <allen-2002@mail.ru>
2021-12-20 22:32:00 +0300

    make linked lists circular

--- dbl_linked_list.py
+++ dbl_linked_list.py
@@ -11,57 +11,46 @@ class DBLLinkedList:
     def append(self, element):
         if not self.head:
             self.head = self.Node(element)
+            self.head.next_node = self.head
+            self.head.prev_node = self.head
             return
 
         node = self.head
-        while node.next_node:
+        while node.next_node != self.head:
+            node = node.next_node
+        new_node = self.Node(element)
+        node.next_node = new_node
+        new_node.prev_node = node
+        new_node.next_node = self.head
+        self.head.prev_node = new_node
+
+    def insert_in_head(self, element):
+        node = self.head
+        if self.head is None:
+            self.append(element)
+            return
+
+        while node.next_node != self.head:
             node = node.next_node
 
         new_node = self.Node(element)
         node.next_node = new_node
+        new_node.next_node = self.head
         new_node.prev_node = node
-
-    def search(self, element):
-        i = 0
-        node = self.head
-
-        while node.next_node:
-            if node.element == element:
-                print(f'Index of element {element}: is {i}')
-                break
-            node = node.next_node
-            i += 1
-        if node.element == element:
-            print(f'Index of element {element}: is {i}')
-            return
-
-    def out(self):
-        node = self.head
-
-        if node != None:
-            while node.next_node:
-                print(node.element)
-                node = node.next_node
-            print(node.element)
-        else:
-            print('List is empty')
+        self.head.prev_node = new_node
+        self.head = new_node
 
     def insert(self, element, index):
         node = self.head
 
         if index == 0:
-            new_node = self.Node(element)
-            new_node.next_node = node
-            self.head = new_node
-            node.prev_node = self.head
+            self.insert_in_head(element)
             return
 
         i = 0
         while i < index:
-            if node.next_node is None and i == index - 1:
-                new_node = self.Node(element=element)
-                node.next_node = new_node
-                new_node.prev_node = node
+            if node.next_node == self.head:
+                self.append(element)
                 return
             node = node.next_node
             i += 1
@@ -72,28 +61,58 @@ class DBLLinkedList:
         node.prev_node.next_node = new_node
         node.prev_node = new_node
 
+    def search(self, element):
+        i = 0
+        node = self.head
+
+        try:
+            while node.next_node != self.head:
+                if node.element == element:
+                    print(f'Index of element {element}: is {i}')
+                    return
+                node = node.next_node
+                i += 1
+            if node.element == element:
+                print(f'Index of element {element}: is {i}')
+                return
+            raise
+        except:
+            print(f'Element {element} not in list')
+
+    def out(self):
+        node = self.head
+
+        if self.head != None:
+            while node.next_node != self.head:
+                print(f"Prev node element: {node.prev_node.element} | "
+                      f"Current node element: {node.element} | "
+                      f"Next node element: {node.next_node.element}")
+                node = node.next_node
+            print(f"Prev node element: {node.prev_node.element} | "
+                  f"Current node element: {node.element} | "
+                  f"Next node element: {node.next_node.element}")
+        else:
+            print('List is empty')
+
     def delete(self, element):
         node = self.head
 
-        index = 0
-        while node.next_node:
+        if self.head.element == element:
+            if self.head.next_node == self.head:
+                self.head = None
+                return
+            while node.next_node != self.head:
+                node = node.next_node
+            node.next_node = self.head.next_node
+            self.head = self.head.next_node
+            self.head.prev_node = node
+            return
+
+        while node.next_node != self.head:
             if node.element == element:
                 break
             node = node.next_node
-            index += 1
 
-        if self.head.next_node is None and index == 0:
-            self.head = None
-            return
-        elif index == 0:
-            self.head.next_node.prev_node = None
-            self.head = self.head.next_node
-            return
-
-        if node.next_node is None:
-            node.prev_node.next_node = None
-            node.prev_node = None
-            return
         node.prev_node.next_node = node.next_node
         node.next_node.prev_node = node.prev_node
-        del node
+        node.next_node = None
--- linked_lists.py
+++ linked_lists.py
@@ -4,7 +4,7 @@ class LinkedList:
 
     class Node:
         def __init__(self, element, next_node=None):
-            self.element = element 
+            self.element = element
             self.next_node = next_node
 
     def append(self, element):
@@ -14,49 +14,79 @@ class LinkedList:
         """
         if not self.head:
             self.head = self.Node(element=element)
+            self.head.next_node = self.head
             return
-        
+
         node = self.head
-        while node.next_node:
+        if node.next_node == self.head:
+            new_node = self.Node(element)
+            node.next_node = new_node
+            new_node.next_node = self.head
+            return
+
+        while node.next_node != self.head:
             node = node.next_node
 
-        node.next_node = self.Node(element=element)
+        new_node = self.Node(element)
+        node.next_node = new_node
+        new_node.next_node = self.head
 
-    def out(self): # Выводим весь список
+    def insert_in_head(self, element):
+        """
+        Добавляем элемент в начало списка
+        :param element: новый элемент который будет вставлен
+        """
         node = self.head
+        if self.head is None:
+            self.append(element)
+            return
 
-        if self.head != None:
-            while node.next_node:
-                print(node.element)
-                node = node.next_node
-            print(node.element)
-        else:
-            print('List is empty')
+        while node.next_node != self.head:
+            node = node.next_node
+
+        new_node = self.Node(element)
+        node.next_node = new_node
+        new_node.next_node = self.head
+        self.head = new_node
 
     def insert(self, element, index):
         """
         Изменяем элемент по конкретному индексу
         :param element: новый элемент который будет вставлен
         :param index: индекс нового элемента
+
+        ПРИМЕЧАНИЕ:
+        Связанный список не предоставляет доступ к элементу по индексу,
+        данная реализация характерна только для массива.
+        Поэтому данный метод было бы неправильно использовать в связанном списке
         """
-        i = 0
         node = self.head
 
         # если индекс элемента = 0 делаем его первым в списке
         if index == 0:
-            new_node = self.Node(element)
-            new_node.next_node = node
-            self.head = new_node
+            self.insert_in_head(element)
             return
 
+        i = 0
         while i < index:
-            prev_node = node # запоминаем прошлый узел
+            prev_node = node  # запоминаем прошлый узел
             node = node.next_node
             i += 1
 
         new_node = self.Node(element=element)
         new_node.next_node = node
         prev_node.next_node = new_node
+
+    def out(self):  # Выводим весь список
+        node = self.head
+
+        if self.head != None:
+            while node.next_node != self.head:
+                print(f"{node.element} | {node.next_node.element}")
+                node = node.next_node
+            print(f"{node.element} | {node.next_node.element}")
+        else:
+            print('List is empty')
 
     def search(self, element):
         """
@@ -67,7 +97,7 @@ class LinkedList:
         node = self.head
 
         try:
-            while node.next_node:
+            while node.next_node != self.head:
                 if node.element == element:
                     print(f'Index of element {element}: is {i}')
                     return
@@ -76,23 +106,32 @@ class LinkedList:
             if node.element == element:
                 print(f'Index of element {element}: is {i}')
                 return
+            raise
         except:
             print(f'Element {element} not in list')
-    
+
     def delete(self, element):
         """
         удаляем элемент из списка и из памяти
         :param element: элемент для удаления
         """
         node = self.head
-
         if self.head.element == element:
-            self.head = node.next_node
+            if self.head.next_node == self.head:
+                self.head = None
+                return
+            while node.next_node != self.head:
+                node = node.next_node
+            node.next_node = self.head.next_node
+            self.head = self.head.next_node
             return
 
-        while node.next_node:
+        while node.next_node != self.head:
             if node.element != element:
                 prev_node = node
+            else:
+                break
             node = node.next_node
         prev_node.next_node = node.next_node
         del node
+
